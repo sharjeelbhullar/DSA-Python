60f76a20e4ae6335fb12f5437d2336b8c0d413d8
sharjeelbhullar <sharjeelbhullar@gmail.com>
2025-01-04 14:40:49 +0500

    Update the code of Bubble sort

--- Sorting/bubble-sort.py
+++ Sorting/bubble-sort.py
@@ -4,3 +4,18 @@ def bubbleSort(self, arr):
         for j in range(i): #loop from 0 to i-1
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+#Optimized
+def bubble_Sort(self, arr):
+    n = len(arr)
+    for i in range(n-1, -1, -1):
+        didSwap = 0
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                didSwap = 1
+        #Check if swap not done in first iteration then end the program as array is already sorted
+        #Because this thing will make the TC more optimized Best [O(N)] if array is sorted 
+        #Worst and average is O(N^2)
+        if didSwap == 0:
+            break
