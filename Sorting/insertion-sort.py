9e522582ef16b0d8a4809378d736a10b3c7fd28a
sharjeelbhullar <sharjeelbhullar@gmail.com>
2025-01-04 21:45:20 +0500

    Add Insertion Sort

---
+++ Sorting/insertion-sort.py
@@ -0,0 +1,7 @@
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        j = i
+        while j>0 and arr[j-1] > arr[j]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
