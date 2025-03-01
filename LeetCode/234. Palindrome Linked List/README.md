ae29221b21f088d2f10b082c26dfef5a8312fa72
sharjeelbhullar <sharjeelbhullar@gmail.com>
2025-03-01 22:06:04 +0500

    Add LeetCode problem 234. Palindrome Linked List

---
+++ LeetCode/234. Palindrome Linked List/README.md
@@ -0,0 +1,29 @@
+# 234. Palindrome Linked List
+
+## Problem Statement
+
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+### Example 1
+
+[](./Example-1.jpg)
+
+Input: head = [1,2,2,1]
+
+Output: true
+
+### Example 2
+
+[](./Example-2.jpg)
+
+Input: head = [1,2]
+
+Output: false
+
+### Constraints
+
+The number of nodes in the list is in the range [1, 105].
+
+0 <= Node.val <= 9
+
+Follow up: Could you do it in O(n) time and O(1) space?
---
+++ LeetCode/234. Palindrome Linked List/main.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return True
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        curr = slow
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        first_half = head
+        second_half = prev
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+
+        curr = prev
+        prev = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        slow.next = prev
+
+        return True
