4eddd0c86fad1e582bdaa815baa7a18dfcbe4a0b
sharjeelbhullar <sharjeelbhullar@gmail.com>
2025-04-17 20:06:21 +0500

    Update 125. Valid Palindrome code

--- LeetCode/125. Valid Palindrome/main.py
+++ LeetCode/125. Valid Palindrome/main.py
@@ -1,14 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) -1
+        l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not self.alphaNum(s[l]):
+            if not self.alphaNum(s[l]):
                 l += 1
-            while r > l and not self.alphaNum(s[r]):
+                continue
+
+            if not self.alphaNum(s[r]):
                 r -= 1
+                continue
+                
             if s[l].lower() != s[r].lower():
                 return False
-            l, r = l+1, r-1
+            l += 1
+            r -= 1
         return True
 
     def alphaNum(self, c):
